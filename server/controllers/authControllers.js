c3a71593ee6043a16d9e1d46265810c84f4ad40b
SergDv1962 <dsv19621227@gmail.com>
2024-06-17 13:30:59 +0300

    add login of the user

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 // Register user
 export const register = async (req, res) => {
@@ -32,3 +33,40 @@ export const register = async (req, res) => {
      res.json({ message: "Помилка при створенні користувача" });
    }
  };
+
+// Login user
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.json({
+        message: "Такого user-а не існує",
+      });
+    }
+    //Порівнюємо закешировані паролі 
+    const isPasswordCorrect = bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res.json({
+        message: "Некоректний пароль",
+      });
+    }
+    // створюємо та шифруємо токін
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30d" }
+    );
+
+    res.json({
+      token,
+      user,
+      message: "Ви успішно увійшли до системи",
+    });
+  } catch (error) {
+    res.json({ message: "Помилка при авторизації" });
+  }
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register } from "../controllers/authControllers.js"
+import { register, login } from "../controllers/authControllers.js"
 
 
 const router = new Router();
@@ -7,5 +7,9 @@ const router = new Router();
 //Register
 //http://localhost:3002/api/auth/register
 router.post('/register', register)
+
+//Login
+//http://localhost:3002/api/auth/login
+router.post('/login', login)
 
 export default router
