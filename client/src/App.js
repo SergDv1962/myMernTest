28ea8339d6946d9a455751eb6bfd61922d118822
SergDv1962 <dsv19621227@gmail.com>
2024-06-17 17:44:32 +0300

    add pages & component. Routing. Login&Registation

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-
+import { Layout } from "./components/Layout";
+import {Routes, Route} from "react-router-dom"
+import { MainPage } from "./pages/MainPage";
+import { PostPage } from "./pages/PostPage";
+import { PostsPage } from "./pages/PostsPage";
+import { AddPostPage } from "./pages/AddPostPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { LoginPage } from "./pages/LoginPage";
+import { EditPostPage } from "./pages/EditPostPage";
 
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<MainPage/>}/>
+        <Route path="posts" element={<PostsPage/>}/>
+        <Route path=":id" element={<PostPage/>}/>
+        <Route path=":id/edit" element={<EditPostPage/>}/>
+        <Route path="new" element={<AddPostPage/>}/>
+        <Route path="register" element={<RegisterPage/>}/>
+        <Route path="login" element={<LoginPage/>}/>
+      </Routes>
+    </Layout>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+  const isAuth = false
+
+  const activeStyle = {
+    color: "white",
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex py-4 justify-between items-center">
+      <span className="flex justify-center items-center w-6 h-6 bg-gray-600 text-xs text-white rounded-sm">
+        E
+      </span>
+      {isAuth && (
+        <ul className="flex gap-8">
+          <li>
+            <NavLink
+              to={"/"}
+              href="/"
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Головна
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/posts"}
+              href="/"
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Мої пости
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/new"}
+              href="/"
+              className="text-xs text-gray-400 hover:text-white"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Додати пост
+            </NavLink>
+          </li>
+        </ul>
+      )}
+
+      <div className="flex justify-center item-center bg-gray-600 text-xs text-white rounded-nm px-4 py-2">
+        {isAuth ? <button>Вийти</button> : <Link to={"/login"}>Увійти</Link>}
+      </div>
+    </div>
+  );
+};
